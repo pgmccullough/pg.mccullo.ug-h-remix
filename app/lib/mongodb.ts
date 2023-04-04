53f156bfc0e281babcc8a1cab2d74e23567e4519
Patrick Glendon McCullough <p@mccullo.ug>
2023-04-04 10:14:31 -0400

    Resolve #15

--- app/lib/mongodb.ts
+++ app/lib/mongodb.ts
@@ -1,5 +1,9 @@
 import { MongoClient, ObjectId } from 'mongodb'
 
+declare global {
+  var __db: MongoClient | undefined;
+}
+
 if (!process.env.MONGODB_URL) {
   throw new Error('Invalid environment variable: "MONGODB_URI"')
 }
@@ -13,7 +17,14 @@ if (!process.env.MONGODB_URL) {
   throw new Error('Please add your Mongo URI to .env.local')
 }
 
-client = new MongoClient(uri)
-clientPromise = client.connect()
+if (process.env.NODE_ENV === "production") {
+  client = new MongoClient(uri);
+  clientPromise = client.connect();
+} else {
+  if(!global.__db) {
+    global.__db = new MongoClient(uri);
+  }
+  clientPromise = global.__db.connect();
+}
 
 export { clientPromise, ObjectId };
