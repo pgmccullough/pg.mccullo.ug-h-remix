a204a5359b8b7c46e5e610ff31d526815816bdc5
Patrick Glendon McCullough <p@mccullo.ug>
2023-08-03 08:50:59 -0400

    patch double /images on video & audio

--- app/components/Media/Video/Video.tsx
+++ app/components/Media/Video/Video.tsx
@@ -4,7 +4,7 @@ export const Video: React.FC<{src:string,alt:string}>  = ({src,alt}) => {
     return (
         <div className="postcard__content_media_video">
             <video width="100%" controls>
-                <source src={`${MEDIA_SERVER_URI}images/${src}`} type="video/mp4" />
+                <source src={`${MEDIA_SERVER_URI}/${src}`} type="video/mp4" />
                 Your browser does not support streaming videos.
             </video>  
         </div>
