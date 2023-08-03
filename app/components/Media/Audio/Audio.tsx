bde2a8aba73aa9eedf457169ccce32adef7baac8
Patrick Glendon McCullough <p@mccullo.ug>
2023-08-03 08:52:47 -0400

    undo that

--- app/components/Media/Audio/Audio.tsx
+++ app/components/Media/Audio/Audio.tsx
@@ -4,7 +4,7 @@ export const Audio: React.FC<{src:string,alt:string}>  = ({src}) => {
     return (
         <div className="postcard__content_media_audio">
             <audio controls>
-                <source src={`${MEDIA_SERVER_URI}/${src}`} type="audio/mpeg" />
+                <source src={`${MEDIA_SERVER_URI}audio/${src}`} type="audio/mpeg" />
                 Your browser does not support streaming videos.
             </audio> 
         </div>
