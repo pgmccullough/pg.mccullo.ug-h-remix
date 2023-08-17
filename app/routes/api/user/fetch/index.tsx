02d4bfefc965d0598804945640afcc93298ac52d
Patrick Glendon McCullough <p@mccullo.ug>
2023-08-17 14:45:08 -0400

    Open get user endpoint

--- app/routes/api/user/fetch/index.tsx
+++ app/routes/api/user/fetch/index.tsx
@@ -1,9 +1,8 @@
-import type { ActionArgs } from "@remix-run/node";
+import type { ActionArgs, LoaderArgs, LoaderFunction } from "@remix-run/node";
 import { clientPromise, ObjectId } from "~/lib/mongodb";
 
-export const action = async ({ request }: ActionArgs) => {
+const getFromId = async(userId: string) => {
   let userObj = null;
-  const userId = (await request.formData()).get("userId")?.toString();
   const client = await clientPromise;
   const db = client.db("user_posts");
   try {
@@ -11,6 +10,19 @@ export const action = async ({ request }: ActionArgs) => {
     return { userObj };
   } catch (err) {
     userObj = null;
-    return { userObj };
+    return { userObj: 'error' };
   }
+}
+
+export const loader: LoaderFunction = async({request}: LoaderArgs) => {
+  const url = new URL(request.url)
+  const userId = url.searchParams.get('userId');
+  const fullUser: any = await getFromId(userId||"");
+  const {cover_image, profile_image, site_description, site_name} = fullUser.userObj;
+  return {cover_image, profile_image, site_description, site_name};
+}
+
+export const action = async ({ request }: ActionArgs) => {
+  const userId = (await request.formData()).get("userId")?.toString();
+  return await getFromId(userId||"");
 }
