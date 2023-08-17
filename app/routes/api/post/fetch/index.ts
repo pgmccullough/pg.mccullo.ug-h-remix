3d7c43d3c9b0a32a334a2707184242e6b72a7bdc
Patrick Glendon McCullough <p@mccullo.ug>
2023-08-17 16:27:18 -0400

    Add public post api endpoint

--- app/routes/api/post/fetch/index.ts
+++ app/routes/api/post/fetch/index.ts
@@ -1,7 +1,21 @@
-import type { ActionArgs } from "@remix-run/node";
+import type { ActionArgs, LoaderFunction, LoaderArgs } from "@remix-run/node";
 
 import { getUser } from "~/utils/session.server";
 import { clientPromise } from "~/lib/mongodb";
+
+export const loader: LoaderFunction = async({request}: LoaderArgs) => {
+  const client = await clientPromise;
+  const db = client.db("user_posts");
+  const url = new URL(request.url)
+  const loadOffset = url.searchParams.get('offset')||0;
+  let additionalPosts;
+  try{
+    additionalPosts = await db.collection("myPosts").find({ privacy : "Public" }).sort({created:-1}).skip(Number(loadOffset)).limit(25).toArray();
+  } catch (err) {
+    additionalPosts = null;
+  }    
+  return { additionalPosts };
+}
 
 export const action = async ({ request }: ActionArgs) => {
   const user = await getUser(request);
